6ceed8eff3a386f02589a7648c179b506e9f7748
lumoe <20421914+lumoe@users.noreply.github.com>
2022-02-21 17:38:07 +0100

    Add streamlit for MC and TD method

---
+++ tictactoe_streamlit_mc.py
@@ -0,0 +1,93 @@
+import streamlit as st
+import numpy as np
+from utils import Dojo, Player, check_game_over_2
+
+
+RL_agent = Player('a_optimal_MC', 0., 0., 0., 0.)
+RL_agent.sign = 1
+
+WON = 1
+ONGOING = 0
+TIE = -1
+
+board = [0] * 9
+move_counter = 1
+game_over = False
+
+
+def show():
+    st.write(
+        """
+        # Machine Learning - Project 3 
+        ## Tic Tac Toe
+        ⁣⭕❕⭕❕❌  
+        ➖➕➖➕➖  
+        ⭕❕⁣❌❕⭕  
+        ➖➕➖➕➖  
+        ❌❕❌❕⭕  
+        ### Interactively play against our RL Agent without training
+        """
+    )
+    st.write("")
+
+    # Initialize state.
+    if "board" not in st.session_state:
+        st.session_state.board = [0] * 9
+        st.session_state.next_player = "X"
+        st.session_state.winner = None
+        st.session_state.game_over = False
+
+
+    state, winner = check_game_over_2(st.session_state.board)
+    print(state, winner)
+
+
+    # print(check_game_over_2(st.session_state.board))
+
+    if st.session_state.next_player == 'O' and state == ONGOING:
+        st.session_state.board = RL_agent.choose_move(st.session_state.board)
+        st.session_state.next_player = 'X'
+
+    # check again after agent move 
+    won, winner = check_game_over_2(st.session_state.board)
+
+    def handle_click(i):
+        if st.session_state.board[i] == 0:
+            st.session_state.board[i] = -1
+            st.session_state.next_player = 'O'
+            print(st.session_state.board)
+
+    # Write rows first 
+    for column in range(0, 3):
+        cols = st.columns(3)
+        for i, row in enumerate(st.session_state.board[column*3:column*3+3]):
+            label = '-'
+            if row == -1:
+                label = 'O'
+            elif row == 1:
+                label = 'X'
+
+            cols[i].button(
+                label,
+                key=f"{i}-{column}",
+                on_click=handle_click,
+                args=(column*3+i,),
+            )
+        
+    if winner != None:
+        if winner == -1:
+            st.success(f"You won against our agent! :party:")
+        elif winner == 1:
+            st.error(f"Our agent is better than yours ")
+
+    if state == TIE:
+        st.info('That\'s a tie. ')
+
+
+    st.write("""
+    #### Project by András Sass, Oto Alves, Lukas Mölschl
+    """)
+
+
+if __name__ == "__main__":
+    show()
--- utils.py
+++ utils.py
@@ -18,6 +18,8 @@ BOARD_SIZE = 9
 choices = [[-1, 0, 1]] * BOARD_SIZE
 all_states = list(product(*choices))
 STATES = [state for state in all_states if abs(sum(list(state))) <= 1]
+EXT_CTR = 0
+# print(STATES)
 
 
 class Player:
@@ -31,7 +33,8 @@ class Player:
                  alpha=0.8,
                  alpha_decay=0,
                  epsilon=0.1,
-                 epsilon_decay=0):
+                 epsilon_decay=0,
+                 initial_value=1):
         """
         :param value_function: A specific value function can be loaded
         by specifying its name. Alternatively "random" can be specified
@@ -45,6 +48,7 @@ class Player:
         """
 
         self.num_games = 0
+        self.update_ctr = 0
         self.sign = 1
         self.alpha = alpha
         self.alpha_decay = alpha_decay
@@ -54,7 +58,7 @@ class Player:
         self.epsilon_decay = epsilon_decay
         self.epsilon_greedy = epsilon
         if value_function == "random":
-            self.value_function = initialize_random_value_function()
+            self.value_function = initialize_random_value_function(initial_value=initial_value)
         else:
             path = "value_functions/" + value_function + "_value_function.pkl"
             file = open(path, "rb")
@@ -87,7 +91,7 @@ class Player:
             chosen_state = random.choice(available_states)
         return list(chosen_state)
 
-    def update_value_function(self, state, next_state):
+    def update_value_function(self, state, next_state, game_over=False, winner=None):
         """
         Using the update: V(S_t) = V(S_t) + learning_rate * (V(S_t+1) - V(S_t))
         with S_t being the current state and S_t+1 the next state,
@@ -112,6 +116,104 @@ class Player:
         file.close()
 
 
+class PlayerMC(Player):
+
+    def __init__(self, value_function="random", alpha=0.8, alpha_decay=0,
+                 epsilon=0.1, epsilon_decay=0, gamma=1):
+        super().__init__(value_function, alpha, alpha_decay,
+                 epsilon, epsilon_decay)
+        self.return_lists = initialize_return_lists()
+        self.return_means = initialize_return_means()
+        self.visited_states_episode = []
+        self.gamma = gamma
+        self.counts = initialize_Ns()
+
+    def update_value_function(self, state, next_state, game_over=False, winner=None):
+        """
+        Using the update: V(S_t) = Average(Returns(S_t))
+        with S_t being the current state and Returns being
+        the list of Returns for the state over the episodes
+        :param state: current state S_t
+        :param next_state: next state S_t+1
+        """
+        if self.alpha_decay != 0:
+            self.learning_rate = np.exp(
+                - self.num_games ** 2 / self.c)  # Gaussian s.t. at game number alpha_decay it is exp(-10)
+
+        self.visited_states_episode.append(state)
+        print('not yet')
+        if check_game_over(state):
+            g = 0
+            for state_idx, state in enumerate(self.visited_states_episode[0:-1]):
+                next_state = self.visited_states_episode[state_idx+1]
+                v_t = self.value_function[state]
+                v_t_1 = self.value_function[next_state]
+                g = self.gamma * g + (v_t_1 - v_t)
+                self.return_lists[state].append(g)
+                self.value_function[state] = np.mean(self.return_lists[state])
+
+            self.visited_states = []  # resetting visited states for next episode
+
+
+class PlayerMCEfficient(PlayerMC):
+
+    def update_value_function(self, state, next_state, game_over=False, winner=None):
+        """
+        Using the update: V(S_t) = Average(Returns(S_t))
+        with S_t being the current state and Returns being
+        the list of Returns for the state over the episodes
+        :param state: current state S_t
+        :param next_state: next state S_t+1
+        """
+        if self.alpha_decay != 0:
+            self.learning_rate = np.exp(
+                - self.num_games ** 2 / self.c)  # Gaussian s.t. at game number alpha_decay it is exp(-10)
+
+        self.visited_states_episode.append(next_state)
+
+        if game_over:
+            g = 0
+            if self.alpha > 0: #micro optimization
+                for state_idx, state in enumerate(self.visited_states_episode[0:-1]):
+                    next_state = self.visited_states_episode[state_idx + 1]
+                    v_t = self.value_function[state]
+                    v_t_1 = self.value_function[next_state]
+                    g = self.gamma * g + (v_t_1 - v_t)
+
+                    # self.counts[state] +=1
+                    # self.value_function[state] = self.value_function[state] + (1/(self.counts[state])) *
+                    # (g - self.value_function[state])
+                    self.value_function[state] = self.value_function[state] + self.learning_rate / 2 * (
+                            g - self.value_function[state])
+
+            self.visited_states_episode = []  # resetting visited states for next episode
+
+
+from minimax import get_best_move
+
+
+class PlayerMinmax(Player):
+    def __init__(self, value_function="random", alpha=0.8, alpha_decay=0,
+                epsilon=0.1, epsilon_decay=0, gamma=1):
+        # Init the class anyway so the has the `self.sign` property
+        super().__init__(value_function, alpha, alpha_decay,
+                epsilon, epsilon_decay)
+
+    def choose_move(self, state):
+        """
+        For a given state a move is returned according to the
+        value function using the minimax algorithm.
+        :param state: The state for which a move should be determined
+        :return: the state after the move
+        """
+        move_position = get_best_move(state, player_sign=self.sign)
+        state[move_position] = self.sign
+        return state
+
+    def update_value_function(self, state, next_state, game_over=False, winner=None):
+        pass
+
+
 class Dojo:
     """
     In a dojo 2 players a and b are present. The players can play against each other
@@ -128,7 +230,10 @@ class Dojo:
                  epsilon_a=0.1,
                  epsilon_b=0.1,
                  epsilon_decay_a=0,
-                 epsilon_decay_b=0):
+                 epsilon_decay_b=0,
+                 algo='TD',
+                 algo2='TD',
+                 gamma=1):
         """
         2 players a and b are instantiated and the the attributes for keeping score are initialized
         :param value_function_a: A specific value function can be loaded
@@ -149,8 +254,22 @@ class Dojo:
         :param epsilon_decay_b: Similar to alpha_decay
         """
 
-        self.player_a = Player(value_function_a, alpha_a, alpha_decay_a, epsilon_a, epsilon_decay_a)
-        self.player_b = Player(value_function_b, alpha_b, alpha_decay_b, epsilon_b, epsilon_decay_b)
+        if algo == 'TD':
+            self.player_a = Player(value_function_a, alpha_a, alpha_decay_a, epsilon_a, epsilon_decay_a)
+        elif algo == 'MC':
+            self.player_a = PlayerMCEfficient(value_function_a, alpha_a, alpha_decay_a, epsilon_a, epsilon_decay_a, gamma)
+        elif algo == 'minimax':
+            self.player_a = PlayerMinmax()
+
+        if algo2 == 'TD':
+            self.player_b = Player(value_function_b, alpha_b, alpha_decay_b, epsilon_b, epsilon_decay_b)
+        elif algo2 == 'MC':
+            self.player_b = PlayerMCEfficient(value_function_b, alpha_b, alpha_decay_b, epsilon_b, epsilon_decay_b, gamma)
+        elif algo2 == 'minimax':
+            self.player_b = PlayerMinmax()
+
+    
+
         self.wins = []
         self.stats = pd.DataFrame(columns=["wins_a", "wins_b", "cum_wins_a", "cum_wins_b", "winrate_a", "winrate_b"])
 
@@ -182,15 +301,15 @@ class Dojo:
                 game_over, winner = check_game_over(board)
                 if move_counter >= 2:
                     # The previous and current afterstates are needed (the state in between is part of the environment)
-                    player.update_value_function(state=boards[-3], next_state=boards[-1])
+                    player.update_value_function(state=boards[-3], next_state=boards[-1], game_over=(game_over & (winner!=0)), winner=winner)
                 if game_over:
                     if winner == 0:
                         boards.append("draw_state")
-                        player.update_value_function(state=boards[-2], next_state=boards[-1])
+                        player.update_value_function(state=boards[-2], next_state=boards[-1], game_over=game_over, winner=winner)
                     else:
                         boards.append("loser_state")
                     player_2 = self.player_b if move_counter % 2 == a_begins else self.player_a
-                    player_2.update_value_function(state=boards[-3], next_state=boards[-1])
+                    player_2.update_value_function(state=boards[-3], next_state=boards[-1], game_over=game_over, winner=winner)
                     player.num_games += 1
                     player_2.num_games += 1
                     if winner == 1:
@@ -238,7 +357,7 @@ class Competition(Dojo):
     A dojo is meant for training (value functions are updated and sometimes random actions
     are taken for exploration sake), but during a competition the strategies of the 2 players are evaluated.
     """
-    def __init__(self, value_function_a="random", value_function_b="random"):
+    def __init__(self, value_function_a="random", value_function_b="random", algo='TD', algo2='TD', gamma=1):
         """
         Initialize the 2 players with 0 learning rates and no chance of selecting random actions
         :param value_function_a: random or loaded by name
@@ -254,11 +373,14 @@ class Competition(Dojo):
                          epsilon_a=0,
                          epsilon_b=0,
                          epsilon_decay_a=0,
-                         epsilon_decay_b=0
+                         epsilon_decay_b=0,
+                         algo=algo,
+                         algo2=algo2,
+                         gamma=gamma
                          )
 
 
-def initialize_random_value_function():
+def initialize_random_value_function(initial_value=1):
     """
     The value function is a dictionary with every possible board as the keys and
     the associated value of that board as the value. Here a random value function
@@ -269,7 +391,7 @@ def initialize_random_value_function():
     Every other state gets a value between 1 and 1.1 (exploring starts)
     :return: The value function as a dictionary
     """
-    values = 1 + np.random.uniform(0, 0.01, len(STATES))
+    values = initial_value + np.random.uniform(0, 0.01, len(STATES))
     state_values = zip(STATES, values)
     value_function = dict(state_values)
     for state in value_function:
@@ -281,6 +403,24 @@ def initialize_random_value_function():
     value_function["loser_state"] = -1
     value_function["draw_state"] = 0
     return value_function
+
+
+def initialize_return_lists():
+    return_lists = dict(zip(STATES, [[] for i in range(len(STATES))]))
+    # print(return_lists)
+    return return_lists
+
+
+def initialize_return_means():
+    return_means = dict(zip(STATES, [(None,0) for i in range(len(STATES))]))
+    # print(return_lists)
+    return return_means
+
+
+def initialize_Ns():
+    Ns = dict(zip(STATES, [0 for i in range(len(STATES))]))
+    # print(return_lists)
+    return Ns
 
 
 def check_game_over(board):
@@ -304,6 +444,7 @@ def check_game_over(board):
         game_over = True
         winner = 0
     return game_over, winner
+
 
 def check_game_over_2(board):
     """
@@ -335,7 +476,6 @@ def check_game_over_2(board):
     return (ONGOING, None)
 
 
-
 def train_test_session(num_train_games=50000,
                        num_test_games=5000,
                        value_function_a="random",
@@ -348,6 +488,9 @@ def train_test_session(num_train_games=50000,
                        epsilon_b=0.1,
                        epsilon_decay_a=50000,
                        epsilon_decay_b=50000,
+                       algo='TD',
+                       algo2=None,
+                       gamma=1,
                        name_value_function_a="a",
                        name_value_function_b="b",
                        moving_average=1000,
@@ -382,6 +525,9 @@ def train_test_session(num_train_games=50000,
     :return: returns the training and testing stats
     """
 
+    if algo2 == None:
+        algo2 = algo
+
     dojo = Dojo(value_function_a=value_function_a,
                 value_function_b=value_function_b,
                 alpha_a=alpha_a,
@@ -391,7 +537,11 @@ def train_test_session(num_train_games=50000,
                 epsilon_a=epsilon_a,
                 epsilon_b=epsilon_b,
                 epsilon_decay_a=epsilon_decay_a,
-                epsilon_decay_b=epsilon_decay_b)
+                epsilon_decay_b=epsilon_decay_b,
+                algo=algo,
+                algo2=algo2,
+                gamma=gamma
+                )
     if num_train_games > 0:
         print("Training:")
         dojo.play(num_train_games)
@@ -400,7 +550,7 @@ def train_test_session(num_train_games=50000,
         dojo.player_a.save_value_function(name=name_value_function_a)
         dojo.player_b.save_value_function(name=name_value_function_b)
 
-    competition = Competition()
+    competition = Competition(algo=algo, algo2=algo2, gamma=gamma)
     competition.player_a.value_function = dojo.player_a.value_function
     competition.player_b.value_function = dojo.player_b.value_function
 
@@ -423,6 +573,8 @@ def average_train_test_sessions(num_experiments=5,
                                 alpha_decay_b=50000,
                                 epsilon_a=0.1,
                                 epsilon_b=0.1,
+                                algo='TD',
+                                gamma=1,
                                 epsilon_decay_a=50000,
                                 epsilon_decay_b=50000,
                                 moving_average=1000,
@@ -467,6 +619,8 @@ def average_train_test_sessions(num_experiments=5,
                                                      epsilon_b=epsilon_b,
                                                      epsilon_decay_a=epsilon_decay_a,
                                                      epsilon_decay_b=epsilon_decay_b,
+                                                     algo=algo,
+                                                     gamma=gamma,
                                                      moving_average=moving_average,
                                                      save=False,
                                                      show_plot=False
@@ -502,3 +656,4 @@ def average_train_test_sessions(num_experiments=5,
             plt.show()
 
     return results
+
